d36a5c83d766159af339eb8a69e1257a69bb3e3c
vaishnav0008 <chardev573@gmail.com>
2025-02-10 12:13:27 +0530

    Add files via upload

--- Concept/PrimeFactorial.java
+++ Concept/PrimeFactorial.java
@@ -7,13 +7,22 @@ class PrimeFactorial
 		System.out.println("Enter the number");
 		int num = sc.nextInt();
 
-		for (int i = 1 ;i<=num ;i++ )
+		for (int i = 2 ;i<=num ;i++ )
 		{
 			if(num%i==0){
-				if(i*i==num){
-					System.out.println("prime factorial");
+				int count = 0;
+				for (int j =2 ;j<i ;j++ )
+				{
+					if (i%j==0)
+					{
+						count ++;
+					}
 				}
-			}
+					if (count==0)
+					{
+						System.out.println(i);
+					}
+				}
 		}
 	}
 }
